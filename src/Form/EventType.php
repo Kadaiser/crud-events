1b9d869c3f70b89b2905dda870a15aa8509b9601
kadaiser <dvalbuen@ucm.es>
2024-04-10 22:44:23 +0200

    added missing files due a inconsistence in devremotecontainers

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,6 +6,9 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormError;
 
 class EventType extends AbstractType
 {
@@ -16,6 +19,16 @@ class EventType extends AbstractType
             ->add('description')
             ->add('location')
             ->add('slots')
+            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+                $eventData = $event->getData();
+                $oldSlots = $event->getForm()->getData()->getSlots();
+                $newSlots = $eventData->getSlots();
+                $usersSuscibed = $event->getForm()->getData()->getUsers()->count();
+
+                if ($newSlots < $usersSuscibed) {
+                    $event->getForm()->get('slots')->addError(new FormError('New slots value cannot be less than the current suscribed users. (currently '.$usersSuscibed.')'));
+                }
+            })
             ->add('endDate')
             // Add more fields as needed
         ;
